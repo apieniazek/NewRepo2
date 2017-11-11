dfbbaec7d35f9b4b11bfd0c802828eb5e8283ec7
Artur <atqqq96@gmail.com>
2017-11-11 15:31:14 +0100

    Wcf Add

---
+++ WcfService2/WcfService2/Service1.svc.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using ClassLibrary1;
+
+namespace WcfService2
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public string SetGame(string title, string kind, int year)
+        {
+            //Game g = new Game(title, kind, year);
+            return string.Format("Title: {0}, Kind: {1}, Year: {2}", title, kind, year);
+        }
+    }
+}
